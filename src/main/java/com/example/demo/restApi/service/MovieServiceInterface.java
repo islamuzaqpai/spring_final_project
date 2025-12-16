846c51987022bbd78c72ec61e73fcd70d7e946ec
islamuzaqpai <iuzakbayy@gmail.com>
2025-12-17 01:18:16 +0500

    add realization of movie service

--- src/main/java/com/example/demo/restApi/service/MovieServiceInterface.java
+++ src/main/java/com/example/demo/restApi/service/MovieServiceInterface.java
@@ -1,5 +1,17 @@
 package com.example.demo.restApi.service;
 
-public interface MovieServiceInterface {
+import com.example.demo.restApi.dto.MovieDto;
 
+import java.util.List;
+
+public interface MovieServiceInterface {
+    MovieDto getMovieById(Long id);
+
+    List<MovieDto> getAllMovies();
+
+    MovieDto addMovie(MovieDto movieDto);
+
+    MovieDto updateMovie(Long id, MovieDto newMovieDto);
+
+    boolean deleteMovie(Long id);
 }
--- src/main/java/com/example/demo/restApi/service/implementation/MovieServiceImpl.java
+++ src/main/java/com/example/demo/restApi/service/implementation/MovieServiceImpl.java
@@ -1,4 +1,64 @@
 package com.example.demo.restApi.service.implementation;
 
-public class MovieServiceImpl {
+import com.example.demo.restApi.dto.MovieDto;
+import com.example.demo.restApi.entity.Movie;
+import com.example.demo.restApi.mapper.MovieMapper;
+import com.example.demo.restApi.repository.MovieRepository;
+import com.example.demo.restApi.service.MovieServiceInterface;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class MovieServiceImpl implements MovieServiceInterface {
+    private final MovieRepository movieRepository;
+    private final MovieMapper movieMapper;
+
+
+    public MovieDto getMovieById(Long id) {
+        return movieMapper.toDto(movieRepository.findById(id).orElse(null));
+    }
+
+    public List<MovieDto> getAllMovies() {
+        return movieMapper.toDtoList(movieRepository.findAll());
+    }
+
+    public MovieDto addMovie(MovieDto movieDto) {
+        return movieMapper.toDto(movieRepository.save(movieMapper.toEntity(movieDto)));
+    }
+
+    public MovieDto updateMovie(Long id, MovieDto newMovieDto) {
+        Movie movie = movieRepository.findById(id).orElse(null);
+
+        if (movie == null) {
+            return null;
+        }
+
+        movie.setTitle(newMovieDto.getTitleDto());
+
+        movie.getActors().clear();
+        movie.setActors(movieMapper.idsToActors(newMovieDto.getActorsIds()));
+
+        movie.setDirector(movieMapper.idToDirector(newMovieDto.getDirectorId()));
+
+        return movieMapper.toDto(movieRepository.save(movie));
+    }
+
+    public boolean deleteMovie(Long id) {
+        Movie movie = movieRepository.findById(id).orElse(null);
+
+        if (movie != null) {
+
+            movie.getActors().clear();
+            movie.setDirector(null);
+
+            movieRepository.delete(movie);
+
+            return true;
+        }
+
+        return false;
+    }
 }
